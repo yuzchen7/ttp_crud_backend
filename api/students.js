41d540f46dbcc4bfd237d39759e755191d10975c
yuzchen7 <yuzchen7@gmail.com>
2023-06-30 03:44:22 +0800

    feat : search student with join campuses data

--- api/students.js
+++ api/students.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { Students } = require('../db/models'); 
+const { Campuses, Students } = require('../db/models'); 
 
 router.get('/', async (req, res, next) => {
     try {
         console.log("student rounter")
-        const allStudent = await Students.findAll();
+        const allStudent = await Students.findAll({ include : Campuses });
+        console.log("allStudent : ", allStudent);
         allStudent ? res.status(200).json(allStudent) : res.status(404).send("Student Data not found");
     } catch (error) {
         next(error);
