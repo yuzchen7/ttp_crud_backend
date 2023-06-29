b517a19d2e2dd9d396ae24513b7feb51011d2b4a
yuzchen7 <yuzchen7@gmail.com>
2023-06-30 01:34:13 +0800

    test : test file for testing db and api

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const express = require('express');
 const db = require('./db');
+// const seed = require('./seed');
+const PORT = "8080";
+
+const app = express();
 
 const syncDB = () => {
     db.sync({force: true});
 };
 
+const severRun = () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+
+severRun();
 syncDB();
 
 module.exports = app;
