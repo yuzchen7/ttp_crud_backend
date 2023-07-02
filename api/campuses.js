089df17bc67a9c367a53ce7ea2c7a689600412e2
yuzchen7 <yuzchen7@gmail.com>
2023-07-02 23:15:57 +0800

    refactor : change campuses find all feat with student count

--- api/campuses.js
+++ api/campuses.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const { Campuses } = require('../db/models');
+const { Campuses, Students } = require('../db/models');
+const db = require('../db');
 
 router.get('/', async (req, res, next) => {
     try {
-        const allCampuses = await Campuses.findAll();
+      console.log('MESSAGE : allCampuses GETTING ALL CAMPUSES INFO FROM DATABASE')
+      const allCampuses = await Campuses.findAll({
+          attributes: [
+              'id',
+              'name',
+              'imageUrl',
+              'address',
+              'description',
+              [db.fn('count', db.col('students.id')), 'studentsNum']
+            ],
+            include: [{
+              model: Students,
+              attributes: []
+            }],
+            group: ['campuses.id']
+          }
+        ).catch(error => {
+            console.error(error);
+        });
         allCampuses ? res.status(200).json(allCampuses) : res.status(404).send("Campuses data not found");
     } catch (error) {
         next(error);
