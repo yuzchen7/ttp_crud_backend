9ff3f61af78c06eadcc03e6641015707e4180a8e
yuzchen7 <yuzchen7@gmail.com>
2023-07-07 03:11:29 +0800

    refactor : set up vercel

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
+require('dotenv').config();
 const database_name = 'backendsql';
 
-const db = new Sequelize(`postgres://localhost:5432/${database_name}`, {
-    logging : false
-});
+const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require");
 
 module.exports = db;
 
--- seed.js
+++ seed.js
@@ -6,15 +6,29 @@ const CampuesesSeed = [
         name : 'Brooklyn College', 
         imageUrl : 'https://www.parchment.com/u/logo/institution/34234.png',
         address : '2900 Bedford Ave, Brooklyn, NY 11210',
-        description : 'very good campuse in the brooklyn area.'
+        description : "Brooklyn College is a public university in Brooklyn in New York City. It is part of the City University of New York system and enrolls over 17,000 undergraduate and over 2,800 graduate students on a 35-acre campus as of 2019. (From wekipideia.com)"
     },
     
     {
         name : 'Hunter College', 
         imageUrl : 'https://d32ogoqmya1dw8.cloudfront.net/images/liberalarts/capstones/hunter/1454363445.v7.jpg',
         address : '695 Park Ave, New York, NY 10065',
-        description : 'very good campuse in the cuny area.'
-    }
+        description : "Hunter College is a public university in New York City. It is one of the constituent colleges of the City University of New York and offers studies in more than one hundred undergraduate and postgraduate fields across five schools. It also administers Hunter College High School and Hunter College Elementary School. (From wekipideia.com)"
+    },
+
+    {
+        name: "John Jay College of Criminal Justice",
+        imageUrl: "https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/home-preview/colleges/layout/admissions-2021-768x402.jpg",
+        address: "524 W 59th St, New York, NY 10019",
+        description: "The John Jay College of Criminal Justice (John Jay) is a public college focused on criminal justice and located in New York City. It is a senior college of the City University of New York (CUNY). John Jay was founded as the only liberal arts college with a criminal justice and forensic focus in the United States. (From wekipideia.com)"
+    },
+
+    {
+        name: "The City College of New York",
+        imageUrl: "https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/home-preview/colleges/layout/City-College-Photo-Request-2017_48_CarlosParker_HarrisHall-768x402.jpg",
+        address: "160 Convent Ave, New York, NY 10031",
+        description: "The City College of the City University of New York (also known as the City College of New York, or simply City College or CCNY) is a public research university within the City University of New York (CUNY) system in New York City. Founded in 1847, City College was the first free public institution of higher education in the United States. It is the oldest of CUNY's 25 institutions of higher learning and is considered its flagship college. (From wekipideia.com)"
+    },
 ];
 
 const StudentsSeed = [
